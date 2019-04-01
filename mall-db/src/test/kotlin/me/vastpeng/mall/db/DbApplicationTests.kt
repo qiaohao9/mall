8955c2104a7ffea89e2bef245f7d7c3323cd47bb
vastpeng <keatingsmitht@gmail.com>
2019-04-02 02:29:14 +0800

    mall-db: init

---
+++ mall-db/src/test/kotlin/me/vastpeng/mall/db/DbApplicationTests.kt
@@ -0,0 +1,16 @@
+package me.vastpeng.mall.db
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DbApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
