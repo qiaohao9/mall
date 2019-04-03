5f7cceee96eec722631ef705f482835438fa23a0
vastpeng <keatingsmitht@gmail.com>
2019-04-03 18:04:51 +0800

    core: complete all

--- mall-core/src/main/kotlin/me/vastpeng/mall/core/CoreApplication.kt
+++ mall-core/src/main/kotlin/me/vastpeng/mall/core/CoreApplication.kt
@@ -1,9 +1,11 @@
 package me.vastpeng.mall.core
 
+import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["me.vastpeng.mall.db", "me.vastpeng.mall.core"])
+@MapperScan("me.vastpeng.mall.db.dao")
 class CoreApplication
 
 fun main(args: Array<String>) {
