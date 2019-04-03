fc2ca4e2403ca3032f2f3d8777e41c6e983c253a
vastpeng <keatingsmitht@gmail.com>
2019-04-03 21:01:51 +0800

    admin-api: add run

--- mall-admin-api/src/main/kotlin/me/vastpeng/mall/adminapi/AdminApiApplication.kt
+++ mall-admin-api/src/main/kotlin/me/vastpeng/mall/adminapi/AdminApiApplication.kt
@@ -1,11 +1,17 @@
 package me.vastpeng.mall.adminapi
 
+import org.mybatis.spring.annotation.MapperScan
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["me.vastpeng.mall.db", "me.vastpeng.mall.core", "me.vastpeng.mall.adminapi"])
+@MapperScan("me.vastpeng.mall.db.dao")
+@EnableTransactionManagement
+@EnableScheduling
 class AdminApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<AdminApiApplication>(*args)
+    SpringApplication.run(AdminApiApplication::class.java, *args)
 }
