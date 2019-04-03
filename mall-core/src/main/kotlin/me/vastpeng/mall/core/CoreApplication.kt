83651a9ebfdcb4d652cbce6f8c74eaaf83b002ce
vastpeng <keatingsmitht@gmail.com>
2019-04-03 15:47:53 +0800

    core: init

---
+++ mall-core/src/main/kotlin/me/vastpeng/mall/core/CoreApplication.kt
@@ -0,0 +1,11 @@
+package me.vastpeng.mall.core
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoreApplication>(*args)
+}
