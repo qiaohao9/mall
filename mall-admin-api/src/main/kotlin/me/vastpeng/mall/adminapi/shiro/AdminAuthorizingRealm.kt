d27f8aa7a051cc842029d8167ad633990e8d59ef
vastpeng <keatingsmitht@gmail.com>
2019-04-03 22:02:07 +0800

    admin-api: shiro

---
+++ mall-admin-api/src/main/kotlin/me/vastpeng/mall/adminapi/shiro/AdminAuthorizingRealm.kt
@@ -0,0 +1,77 @@
+package me.vastpeng.mall.adminapi.shiro
+
+import me.vastpeng.mall.core.util.bcrypt.BCryptPasswordEncoder
+import me.vastpeng.mall.db.domain.MallAdmin
+import me.vastpeng.mall.db.service.MallAdminService
+import me.vastpeng.mall.db.service.MallPermissionService
+import me.vastpeng.mall.db.service.MallRoleService
+import org.apache.shiro.authc.*
+import org.apache.shiro.authz.AuthorizationException
+import org.apache.shiro.authz.AuthorizationInfo
+import org.apache.shiro.authz.SimpleAuthorizationInfo
+import org.apache.shiro.realm.AuthorizingRealm
+import org.apache.shiro.subject.PrincipalCollection
+import org.apache.shiro.util.Assert
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
+import org.springframework.util.StringUtils
+import javax.annotation.Resource
+
+@Configuration
+class AdminAuthorizingRealm : AuthorizingRealm() {
+
+    companion object {
+        private val log = LoggerFactory.getLogger(AdminAuthorizingRealm::class.java)
+    }
+
+    @Autowired
+    private lateinit var adminService: MallAdminService
+    @Autowired
+    private lateinit var roleService: MallRoleService
+    @Autowired
+    private lateinit var permissionService: MallPermissionService
+
+    override fun doGetAuthenticationInfo(token: AuthenticationToken?): AuthenticationInfo {
+        var upToken = token as UsernamePasswordToken
+        var username = upToken.username
+        var password = String(upToken.password)
+
+        if (StringUtils.isEmpty(username)) {
+            throw AccountException("用户名不能为空");
+        }
+        if (StringUtils.isEmpty(password)) {
+            throw AccountException("密码不能为空")
+        }
+
+        var adminList = adminService.findAdmin(username)
+        Assert.state(adminList.size < 2, "同一个用户名存在两个账户")
+        if (adminList.isEmpty()) {
+            throw UnknownAccountException("找不到用户($username)的账号信息")
+        }
+        var admin = adminList[0]
+
+        var encoder = BCryptPasswordEncoder()
+        if (!encoder.matches(password, admin.password)) {
+            throw UnknownAccountException("找不到用户($username)的账号信息")
+        }
+
+        return SimpleAuthenticationInfo(admin, password, name)
+    }
+
+    override fun doGetAuthorizationInfo(principals: PrincipalCollection?): AuthorizationInfo {
+        if (principals == null) {
+            throw AuthorizationException("PrincipalCollection method argument cannot be null.")
+        }
+
+        val admin = getAvailablePrincipal(principals) as MallAdmin
+        var roleIds = admin.roleIds
+        var roles = roleService.queryByIds(roleIds)
+        var permissions = permissionService.queryByRoleIds(roleIds)
+        var info = SimpleAuthorizationInfo()
+        info.roles = roles
+        info.stringPermissions = permissions
+        return info
+    }
+
+}
---
+++ mall-admin-api/src/main/kotlin/me/vastpeng/mall/adminapi/shiro/AdminWebSessionManager.kt
@@ -0,0 +1,31 @@
+package me.vastpeng.mall.adminapi.shiro
+
+import org.apache.commons.lang3.StringUtils
+import org.apache.shiro.web.servlet.ShiroHttpServletRequest
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager
+import org.apache.shiro.web.util.WebUtils
+import java.io.Serializable
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+
+
+class AdminWebSessionManager : DefaultWebSessionManager() {
+    companion object {
+        const val LOGIN_TOKEN_KEY = "X-Litemall-Admin-Token"
+        private const val REFERENCED_SESSION_ID_SOURCE = "Stateless request"
+    }
+
+    override fun getSessionId(request: ServletRequest, response: ServletResponse): Serializable {
+        val id = WebUtils.toHttp(request).getHeader(LOGIN_TOKEN_KEY)
+        if (!StringUtils.isEmpty(id)) {
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_SOURCE, REFERENCED_SESSION_ID_SOURCE)
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID, id)
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_IS_VALID, java.lang.Boolean.TRUE)
+            return id
+        } else {
+            return super.getSessionId(request, response)
+        }
+    }
+
+
+}
